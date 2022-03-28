03917e679fc88e8c7228ecaa6ecb89b47802ddcf
enaburekhan <ericenaburekhan2018@gmail.com>
2022-03-28 23:13:05 +0100

    Create Task model and add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Task.create(title: 'Clean the house', completed: false)
+Task.create(title: 'Walk out the dog', completed: true)
+Task.create(title: 'Wash the dishes', completed: false)
