016b82513a7f8e082fc165d16908b37bd7cc3475
enaburekhan <ericenaburekhan2018@gmail.com>
2022-03-28 23:26:11 +0100

    Create task controller and render the view

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,8 @@
+class TasksController < ApplicationController
+  layout false
+
+  def index
+    @tasks = Task.order('created_at DESC')
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  Rails.application.routes.draw do
+    resources :tasks, only: %i[index]
+  end
 end
