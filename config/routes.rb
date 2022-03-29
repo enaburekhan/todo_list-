da9a73fa21de7262f7f833aab5a8f7014b91d695
enaburekhan <ericenaburekhan2018@gmail.com>
2022-03-29 08:01:04 +0100

    Create task_controller.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 
-  Rails.application.routes.draw do
-    resources :tasks, only: %i[index]
-  end
+  root 'homes#index'
+  get 'tasks/index'
+  resources :tasks, only: %i[index]
+
 end
