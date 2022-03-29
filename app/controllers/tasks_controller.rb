f94c37c4cb338db6beadd7842c579d9129bf3777
enaburekhan <ericenaburekhan2018@gmail.com>
2022-03-29 08:19:53 +0100

    Refactor task_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,23 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.order('created_at DESC')
+    @task = Task.new
   end
   
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def complete
+    @task = Task.find(params[:id])
+    @task.update(completed: true)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'homes#index'
+  root to: 'homes#index'
   get 'tasks/index'
-  resources :tasks, only: %i[index]
 
+  resources :tasks, only: %i[index create] do
+
+    member do
+      get 'complete'
+    end
+  end
 end
